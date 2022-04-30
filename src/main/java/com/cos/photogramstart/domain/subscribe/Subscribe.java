4a5410f79820fe0ab362cb721ff13930a29d81d6
sorayeon <jihwan77@gmail.com>
2022-04-30 21:54:34 +0900

    구독하기 - 모델만들기

---
+++ src/main/java/com/cos/photogramstart/domain/subscribe/Subscribe.java
@@ -0,0 +1,45 @@
+package com.cos.photogramstart.domain.subscribe;
+// 구독 테이블 유저 N : N 유저
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "subscribe_uk",
+                        columnNames = {"fromUserId", "toUserId"}
+                )
+        }
+)
+public class Subscribe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "fromUserId") // 이렇게 컬럼명 만들어!
+    @ManyToOne
+    private User fromUser;
+
+    @JoinColumn(name = "toUserId")
+    @ManyToOne
+    private User toUser;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
