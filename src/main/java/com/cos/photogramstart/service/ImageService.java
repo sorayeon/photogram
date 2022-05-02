158db6726633d05ef44679686746f909e19a682f
sorayeon <jihwan77@gmail.com>
2022-05-02 22:28:14 +0900

    프로필페이지 - @Transactional
    READ 만 있는 메세드는 readOnly=true 읽기 전용 모드
    
    예상치 못한 엔티티의 CUD를 예방, 성능 최적화 (세션 플러시 모드를 MANUAL 로 설정)

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,6 +25,7 @@ public class ImageService {
     @Value("${file.path}")
     private String uploadFolder;
 
+    @Transactional
     public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
         UUID uuid = UUID.randomUUID();
         String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename(); // 1.jpg
@@ -42,8 +44,7 @@ public class ImageService {
         // image 테이블에 저장
         Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser());
 
-        Image imageEntity = imageRepository.save(image);
+        imageRepository.save(image);
 
-        log.info(imageEntity.toString());
     }
 }
