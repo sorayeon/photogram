c7c2c48c32d4923f5ff51f6b36b5059d56cfef15
sorayeon <jihwan77@gmail.com>
2022-05-28 10:08:19 +0900

    댓글 - 댓글쓰기 AJAX 구현

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -60,7 +60,7 @@ function getStoryItem(image) {
 		  <p>${image.caption || ''}</p>
 		</div>
 	
-		<div id="storyCommentList-1">
+		<div id="storyCommentList-${image.id}">
 	
 		  <div class="sl__item__contents__comment" id="storyCommentItem-1"">
 			<p>
@@ -76,8 +76,8 @@ function getStoryItem(image) {
 		</div>
 	
 		<div class="sl__item__input">
-		  <input type="text" placeholder="댓글 달기..." id="storyCommentInput-1" />
-		  <button type="button" onClick="addComment()">게시</button>
+		  <input type="text" placeholder="댓글 달기..." id="storyCommentInput-${image.id}" />
+		  <button type="button" onClick="addComment(${image.id})">게시</button>
 		</div>
 	
 	  </div>
@@ -138,12 +138,13 @@ function toggleLike(imageId) {
 }
 
 // (4) 댓글쓰기
-function addComment() {
+function addComment(imageId) {
 
-	let commentInput = $("#storyCommentInput-1");
-	let commentList = $("#storyCommentList-1");
+	let commentInput = $("#storyCommentInput-" + imageId);
+	let commentList = $("#storyCommentList-" + imageId);
 
 	let data = {
+		imageId: imageId,
 		content: commentInput.val()
 	}
 
@@ -151,6 +152,18 @@ function addComment() {
 		alert("댓글을 작성해주세요!");
 		return;
 	}
+
+	$.ajax({
+		type: "post",
+		url: "/api/comment",
+		data: JSON.stringify(data),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json"
+	}).done(res => {
+		console.log("성공", res);
+	}).fail(error => {
+		console.log("에러", error);
+	});
 
 	let content = `
 			  <div class="sl__item__contents__comment" id="storyCommentItem-2""> 
