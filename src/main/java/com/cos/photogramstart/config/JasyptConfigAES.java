104d506258b4d15e7d3a16403f027fa78f84af22
sorayeon <jihwan77@gmail.com>
2022-10-18 00:46:14 +0900

    AWS S3 파일업로드 설정정보 추가 및 암호화

--- src/main/java/com/cos/photogramstart/config/JasyptConfigAES.java
+++ src/main/java/com/cos/photogramstart/config/JasyptConfigAES.java
@@ -4,19 +4,22 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfigAES {
+    @Value("${jasypt.encryptor.password}")
+    private String password;
 
     @Bean("jasyptEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         encryptor.setProvider(new BouncyCastleProvider());
         encryptor.setPoolSize(2);
-        encryptor.setPassword("P@ssw0rd");
+        encryptor.setPassword(password);
         encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
         return encryptor;
     }
